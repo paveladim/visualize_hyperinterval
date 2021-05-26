2c8766fcf7438a20f523b6d2aff88bb04304c728
pavel petrov <paveladim@yandex.ru>
2021-05-26 21:31:56 +0300

    fc

---
+++ visual_hyperinterval/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace visual_hyperinterval
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.build = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(649, 649);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // build
+            // 
+            this.build.Location = new System.Drawing.Point(690, 340);
+            this.build.Name = "build";
+            this.build.Size = new System.Drawing.Size(75, 23);
+            this.build.TabIndex = 1;
+            this.build.Text = "Построить";
+            this.build.UseVisualStyleBackColor = true;
+            this.build.Click += new System.EventHandler(this.build_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 678);
+            this.Controls.Add(this.build);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button build;
+    }
+}
+
---
+++ visual_hyperinterval/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace visual_hyperinterval
+{
+    public partial class Form1 : Form
+    {
+        private uint MAX_EXPONENT_THREE = 3486784401;
+        private List<uint> Points = new List<uint>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void build_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\materials\projects\visual_hyperinterval\points.txt";
+
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Points.Add(uint.Parse(line));
+                }
+            }
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            System.Drawing.Rectangle rect;
+            rect = new Rectangle(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            Pen blackPen = new Pen(Color.Black);
+            Pen bluePen = new Pen(Color.Blue);
+            gr.FillRectangle(whiteBrush, rect);
+            gr.DrawRectangle(blackPen, rect);
+
+            int u1 = 0;
+            int u2 = 0;
+
+            for (int i = 0; i < Points.Count - 1; i += 2)
+            {
+                // считываем точку из списка и приводим её к координатам pictureBox1
+                u1 = (int)(Points[i] * (pictureBox1.Width - 1) / MAX_EXPONENT_THREE);
+                u2 = (int)(Points[i + 1] * (pictureBox1.Height - 1) / MAX_EXPONENT_THREE);
+
+                // выполняем преобразование поворота и сдвиг
+                u1 = -u1 + (pictureBox1.Width - 1);
+
+                gr.DrawEllipse(bluePen, u1 - 5, u2 - 5, 2.0f * 5, 2.0f * 5);
+                gr.FillEllipse(redBrush, u1 - 5, u2 - 5, 2 * 5, 2 * 5);
+                gr.DrawEllipse(blackPen, u1 - 5, u2 - 5, 2 * 5, 2 * 5);
+            }
+        }
+    }
+}
