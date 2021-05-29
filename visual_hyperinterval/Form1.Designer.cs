ad962a117ec131263d5d0202a782d62cafaa94f1
pavel petrov <paveladim@yandex.ru>
2021-05-29 20:28:27 +0300

    drawing intervals

--- visual_hyperinterval/Form1.Designer.cs
+++ visual_hyperinterval/Form1.Designer.cs
@@ -31,12 +31,17 @@ namespace visual_hyperinterval
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.build = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(56, 13);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(649, 649);
             this.pictureBox1.TabIndex = 0;
@@ -46,7 +51,7 @@ namespace visual_hyperinterval
             // 
             // build
             // 
-            this.build.Location = new System.Drawing.Point(690, 340);
+            this.build.Location = new System.Drawing.Point(733, 341);
             this.build.Name = "build";
             this.build.Size = new System.Drawing.Size(75, 23);
             this.build.TabIndex = 1;
@@ -54,17 +59,68 @@ namespace visual_hyperinterval
             this.build.UseVisualStyleBackColor = true;
             this.build.Click += new System.EventHandler(this.build_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(720, 665);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(692, 665);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 665);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 678);
+            this.ClientSize = new System.Drawing.Size(833, 687);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.build);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +128,11 @@ namespace visual_hyperinterval
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button build;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- visual_hyperinterval/Form1.cs
+++ visual_hyperinterval/Form1.cs
@@ -15,6 +15,7 @@ namespace visual_hyperinterval
     {
         private uint MAX_EXPONENT_THREE = 3486784401;
         private List<uint> Points = new List<uint>();
+        private List<uint> PointsHyp = new List<uint>();
         public Form1()
         {
             InitializeComponent();
@@ -22,14 +23,24 @@ namespace visual_hyperinterval
 
         private void build_Click(object sender, EventArgs e)
         {
-            string path = @"D:\materials\projects\visual_hyperinterval\points.txt";
+            string path1 = @"D:\materials\projects\visual_hyperinterval\points.txt";
+            string path2 = @"D:\materials\projects\visual_hyperinterval\hyp.txt";
 
-            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            using (StreamReader sr1 = new StreamReader(path1, System.Text.Encoding.Default))
             {
-                string line;
-                while ((line = sr.ReadLine()) != null)
+                string line1;
+                while ((line1 = sr1.ReadLine()) != null)
                 {
-                    Points.Add(uint.Parse(line));
+                    Points.Add(uint.Parse(line1));
+                }
+            }
+
+            using (StreamReader sr2 = new StreamReader(path2, System.Text.Encoding.Default))
+            {
+                string line2;
+                while ((line2 = sr2.ReadLine()) != null)
+                {
+                    PointsHyp.Add(uint.Parse(line2));
                 }
             }
 
@@ -54,6 +65,54 @@ namespace visual_hyperinterval
 
             int u1 = 0;
             int u2 = 0;
+            int v1 = 0;
+            int v2 = 0;
+
+            for (int i = 0; i < PointsHyp.Count - 3; i += 4)
+            {
+                // считываем точку из списка и приводим её к координатам pictureBox1
+                u1 = (int)(PointsHyp[i] * (pictureBox1.Width - 1) / MAX_EXPONENT_THREE);
+                u2 = (int)(PointsHyp[i + 1] * (pictureBox1.Height - 1) / MAX_EXPONENT_THREE);
+                v1 = (int)(PointsHyp[i + 2] * (pictureBox1.Width - 1) / MAX_EXPONENT_THREE);
+                v2 = (int)(PointsHyp[i + 3] * (pictureBox1.Height - 1) / MAX_EXPONENT_THREE);
+
+                // выполняем преобразование поворота и сдвиг
+                u2 = -u2 + (pictureBox1.Height - 1);
+                v2 = -v2 + (pictureBox1.Height - 1);
+
+                if ((u1 < v1) && (u2 < v2))
+                {
+                    rect = new Rectangle(u1, u2, v1 - u1, v2 - u2);
+                    gr.FillRectangle(whiteBrush, rect);
+                    gr.DrawRectangle(blackPen, rect);
+                }
+                else if ((v1 < u1) && (v2 < u2))
+                {
+                    rect = new Rectangle(v1, v2, u1 - v1, u2 - v2);
+                    gr.FillRectangle(whiteBrush, rect);
+                    gr.DrawRectangle(blackPen, rect);
+                } 
+                else if ((u1 < v1) && (u2 > v2))
+                {
+                    int tmp = u1;
+                    u1 = v1;
+                    v1 = tmp;
+
+                    rect = new Rectangle(v1, v2, u1 - v1, u2 - v2);
+                    gr.FillRectangle(whiteBrush, rect);
+                    gr.DrawRectangle(blackPen, rect);
+                }
+                else if ((u1 > v1) && (u2 < v2))
+                {
+                    int tmp = u1;
+                    u1 = v1;
+                    v1 = tmp;
+
+                    rect = new Rectangle(u1, u2, v1 - u1, v2 - u2);
+                    gr.FillRectangle(whiteBrush, rect);
+                    gr.DrawRectangle(blackPen, rect);
+                }
+            }
 
             for (int i = 0; i < Points.Count - 1; i += 2)
             {
@@ -64,9 +123,9 @@ namespace visual_hyperinterval
                 // выполняем преобразование поворота и сдвиг
                 u2 = -u2 + (pictureBox1.Height - 1);
 
-                gr.DrawEllipse(bluePen, u1 - 5, u2 - 5, 2.0f * 5, 2.0f * 5);
-                gr.FillEllipse(redBrush, u1 - 5, u2 - 5, 2 * 5, 2 * 5);
-                gr.DrawEllipse(blackPen, u1 - 5, u2 - 5, 2 * 5, 2 * 5);
+                gr.DrawEllipse(bluePen, u1 - 3, u2 - 3, 2.0f * 3, 2.0f * 3);
+                gr.FillEllipse(redBrush, u1 - 3, u2 - 3, 2 * 3, 2 * 3);
+                gr.DrawEllipse(blackPen, u1 - 3, u2 - 3, 2 * 3, 2 * 3);
             }
         }
     }
